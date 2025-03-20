a383deca760a812197c7d26177ca886bab553400
Snehal Lavangare <snehallavangare19@gmail.com>
2025-03-20 08:10:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <div>
+        3D developer
+      </div>
+
+      <h1 class="text-3xl underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
