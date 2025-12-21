f6a1e76015883bc3bf6aefd735341f764584a353
Snehal Lavangare <snehallavangare19@gmail.com>
2025-12-21 15:25:52 +0530

    Fixed the breaking transition.

--- src/routes/KineticCarousel.jsx
+++ src/routes/KineticCarousel.jsx
@@ -16,7 +16,7 @@ const KineticCarousel = () => {
 
     wrapperRef.current.style.setProperty(
       "--transition",
-      "600ms cubic-bezier(0.22, 0.61, 0.36, 1)"
+      "300ms cubic-bezier(0.22, 0.61, 0.36, 1)"
     );
 
     timeoutRef.current = setTimeout(() => {
@@ -38,7 +38,7 @@ const KineticCarousel = () => {
             <li
               aria-current={activeIndex === index}
               key={image.id}
-              onMouseEnter={() => setActiveIndex(index)}
+              onMouseOver={() => setActiveIndex(index)}
               className={classNames(
                 "relative w-[8%] first:w-[1%] last:w-[1%] [&[aria-current='true']]:w-[48%]",
                 "before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-[-6px] before:right-[-6px] before:z-0",
@@ -50,9 +50,11 @@ const KineticCarousel = () => {
               <img
                 src={image.src}
                 alt={image.alt}
-                width={640}
-                height={590}
-                className="absolute z-10 w-[640px] h-[590px] object-cover block max-w-none left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                width="590"
+                height="640"
+                loading="eager"
+                decoding="async"
+                className="absolute z-10 w-[640px] h-[590px] object-cover block max-w-none left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform-gpu transition-transform duration-200 ease-out"
               />
             </div>
             </li>
